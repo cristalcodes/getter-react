162b1dd5866b7d791eb8ef3257630e58317343ce
Cristal Alatorre Perez <cristalcodes@gmail.com>
2020-03-10 23:21:51 -0400

    Adds imports for Provider, createStore, applyMiddleware, thunk, and BrowserRouter

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'react-redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
 ReactDOM.render(
