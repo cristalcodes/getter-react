600104ffd65352659a63918ac130af44c41f27b3
Cristal Alatorre Perez <cristalcodes@gmail.com>
2020-03-14 18:56:27 -0400

    Adds Navigation bar using material-ui

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,19 +2,30 @@ import React from "react";
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import Home from "../components/home";
 import GetterApp from "../components/GetterApp";
-
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 const Navbar = () =>(
-  <div>
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="/decks">Decks</NavLink>
-  </div>
+  <div >
+      <AppBar position="static">
+        <Toolbar>
+
+          <Typography variant="h6" >
+            <NavLink to="/" style={{color: 'white', textDecoration: 'none', marginRight: 10}} >Home</NavLink>
+          </Typography>
+          <Typography variant="h6">
+              <NavLink to="/decks" style={{color: 'white', textDecoration: 'none', marginRight:10}} >Decks</NavLink>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+
 )
 
 
 const AppRouter = () => {
   return(
-
     <Router>
       <div>
         <Navbar />
@@ -22,8 +33,6 @@ const AppRouter = () => {
         <Route exact path="/decks" component={GetterApp} />
       </div>
     </Router>
-
-
   )
 }
 
