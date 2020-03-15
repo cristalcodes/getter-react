770536df616dffe918272e1bc84058d504469c9e
Cristal Alatorre Perez <cristalcodes@gmail.com>
2020-03-15 00:21:31 -0400

    Centers the clock on the homepage.

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import Clock from 'react-live-clock';
 import ReactFitText from 'react-fittext';
+import styled from 'styled-components';
+
+const Container= styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
 
 const home = () => {
   return(
     <div>
-    <br/>
-    Clock is ticking... what should you be working on?
-    <ReactFitText compressor={0.5}>
+    <Container>
+
+    <ReactFitText compressor={0.25}>
       <h1>
         <Clock format="h:mm:ss" interval={1000} ticking={true} />
       </h1>
     </ReactFitText>
-
+    <h5><center>Clock is ticking... what should you be working on?</center></h5>
+    </Container>
     </div>
   )
 }
